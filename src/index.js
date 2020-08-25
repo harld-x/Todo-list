91917b2a1ba53463afcfbee5acabd20e5559547f
harld-x <danilyudin723@gmail.com>
2020-08-25 17:26:42 +0700

    changed

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import TodoList from "./components/todo-list";
 import SearchPanel from "./components/search-panel";
 const App = () => {
 
+    const todoDate = [
+        {label: 'Drink Coffee', important: false},
+        {label: 'Make Awesome App', important: true},
+        {label: 'Have a lunch', important: false}
+    ]
+
     return (
         <div>
             <AppHeader/>
             <SearchPanel/>
-            <TodoList/>
+            <TodoList todos={todoDate}/>
         </div>
     )
 }
